8b7abda7f205cbd061eedc8f42227204a064208e
quangharyanto <apple.haryanto@gmail.com>
2019-04-29 10:56:43 +0700

    refactored storyboard bmi and second

---
+++ BMICalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMICalculator
+//
+//  Created by Haryanto Salim on 24/04/19.
+//  Copyright © 2019 Haryanto Salim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
